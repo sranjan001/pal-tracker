0eb29a81d1a670b93f4b22fdc509a22a6d667728
Tyson Gern <tgern@pivotal.io>
2017-07-26 12:28:32 -0600

    Actuator

---
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryHealthIndicator.java
@@ -0,0 +1,13 @@
+package test.pivotal.pal.tracker;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TimeEntryHealthIndicator implements HealthIndicator {
+    @Override
+    public Health health() {
+        return null;
+    }
+}
